8fc2bb30855f5ff418202026f18d6dcf023157e7
Muhammad Yudi Setiawan <yudsetiawann@gmail.com>
2025-12-23 23:04:26 +0700

    delete Participant menu

--- app/Filament/Resources/Participants/ParticipantResource.php
+++ app/Filament/Resources/Participants/ParticipantResource.php
@@ -1,58 +1,58 @@
 <?php
 
-namespace App\Filament\Resources\Participants;
+// namespace App\Filament\Resources\Participants;
 
-use UnitEnum;
-use BackedEnum;
-use Filament\Tables\Table;
-use App\Models\Participant;
-use Filament\Schemas\Schema;
-use Filament\Resources\Resource;
-use Filament\Support\Icons\Heroicon;
-use Illuminate\Support\Facades\Auth;
-use App\Filament\Resources\Participants\Pages\EditParticipant;
-use App\Filament\Resources\Participants\Pages\ListParticipants;
-use App\Filament\Resources\Participants\Pages\CreateParticipant;
-use App\Filament\Resources\Participants\Schemas\ParticipantForm;
-use App\Filament\Resources\Participants\Tables\ParticipantsTable;
+// use UnitEnum;
+// use BackedEnum;
+// use Filament\Tables\Table;
+// use App\Models\Participant;
+// use Filament\Schemas\Schema;
+// use Filament\Resources\Resource;
+// use Filament\Support\Icons\Heroicon;
+// use Illuminate\Support\Facades\Auth;
+// use App\Filament\Resources\Participants\Pages\EditParticipant;
+// use App\Filament\Resources\Participants\Pages\ListParticipants;
+// use App\Filament\Resources\Participants\Pages\CreateParticipant;
+// use App\Filament\Resources\Participants\Schemas\ParticipantForm;
+// use App\Filament\Resources\Participants\Tables\ParticipantsTable;
 
-class ParticipantResource extends Resource
-{
-    protected static ?string $model = Participant::class;
-    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-user-group';
-    protected static ?string $navigationLabel = 'Manajemen Anggota';
-    protected static string|UnitEnum|null $navigationGroup = 'Data Anggota';
-    protected static ?string $recordTitleAttribute = 'name';
+// class ParticipantResource extends Resource
+// {
+//     protected static ?string $model = Participant::class;
+//     protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-user-group';
+//     protected static ?string $navigationLabel = 'Manajemen Anggota';
+//     protected static string|UnitEnum|null $navigationGroup = 'Data Anggota';
+//     protected static ?string $recordTitleAttribute = 'name';
 
-    public static function form(Schema $schema): Schema
-    {
-        return ParticipantForm::configure($schema);
-    }
+//     public static function form(Schema $schema): Schema
+//     {
+//         return ParticipantForm::configure($schema);
+//     }
 
-    public static function table(Table $table): Table
-    {
-        return ParticipantsTable::configure($table);
-    }
+//     public static function table(Table $table): Table
+//     {
+//         return ParticipantsTable::configure($table);
+//     }
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
+//     public static function getRelations(): array
+//     {
+//         return [
+//             //
+//         ];
+//     }
 
-    public static function getPages(): array
-    {
-        return [
-            'index' => ListParticipants::route('/'),
-            'create' => CreateParticipant::route('/create'),
-            'edit' => EditParticipant::route('/{record}/edit'),
-        ];
-    }
+//     public static function getPages(): array
+//     {
+//         return [
+//             'index' => ListParticipants::route('/'),
+//             'create' => CreateParticipant::route('/create'),
+//             'edit' => EditParticipant::route('/{record}/edit'),
+//         ];
+//     }
 
-    public static function canViewAny(): bool
-    {
-        // Hanya izinkan jika user adalah admin
-        return Auth::user()->isAdmin();
-    }
-}
+//     public static function canViewAny(): bool
+//     {
+//         // Hanya izinkan jika user adalah admin
+//         return Auth::user()->isAdmin();
+//     }
+// }
